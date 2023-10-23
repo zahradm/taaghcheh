0fa54e7d9688433f72020f9d3692e6aa6253de74
zahradm <zh.dehghani741@gmail.com>
2023-10-23 21:55:41 +0330

    update views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from main.serializers import BookNameInputSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+from utils.preprocessing import mean_rating
+
+
+class MeanRateAPI(APIView):
+    serializer_class = BookNameInputSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        mean_rate = mean_rating(data['book_name'])
+        return Response({'success': True, 'results': mean_rate})
