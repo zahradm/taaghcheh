3c0892cf0ea9a521c77f526cf3f34b68739cf560
zahradm <zh.dehghani741@gmail.com>
2023-10-23 21:54:32 +0330

    add utils

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-data = pd.read_csv('../../data/taghche.csv')
+data = pd.read_csv('/media/zahra/06D8A5DBD8A5C8EF/Projects/taaghcheh/data/taghche.csv')
 
 
 def mean_rating(book_name):
     filtered_data = data[data['bookname'] == book_name]
     return filtered_data['rate'].mean()
-
-
-mean_rating('آشپزخانه')
