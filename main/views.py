fcc33f916a2939ae370b4c81a7ab3dc8f20a8c9c
zahradm <zh.dehghani741@gmail.com>
2023-10-23 22:23:31 +0330

    sort import

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from main.serializers import BookNameInputSerializer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from main.serializers import BookNameInputSerializer
 from utils.preprocessing import mean_rating
 
 
