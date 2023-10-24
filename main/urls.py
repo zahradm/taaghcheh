33c947a57baf62ddc095b13ea8267665c41b9fa0
zahradm <zh.dehghani741@gmail.com>
2023-10-24 23:34:39 +0330

    add requent-word url

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import MeanRateAPI
+from main.views import MeanRateAPI, FrequentWordAPI
 
 app_name = 'main'
 
 urlpatterns = [
-    path('rate/', MeanRateAPI.as_view(), name='rate')
+    path('rate/', MeanRateAPI.as_view(), name='rate'),
+    path('frequent-word/', FrequentWordAPI.as_view(), name='frequent-word')
 ]
