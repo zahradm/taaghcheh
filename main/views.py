17c04bef887b8c17261f10c238539b046b181ff0
zahradm <zh.dehghani741@gmail.com>
2023-10-24 23:33:53 +0330

    add FrequentWordAPI

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from main.serializers import BookNameInputSerializer
-from utils.preprocessing import mean_rating
+from utils.preprocessing import mean_rating, frequent_word
 
 
 class MeanRateAPI(APIView):
@@ -15,3 +15,14 @@ class MeanRateAPI(APIView):
         data = serializer.validated_data
         mean_rate = mean_rating(data['book_name'])
         return Response({'success': True, 'results': mean_rate})
+
+
+class FrequentWordAPI(APIView):
+    serializer_class = BookNameInputSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        frequent_word_comment = frequent_word(data['book_name'])
+        return Response({'success': True, 'results': frequent_word_comment})
